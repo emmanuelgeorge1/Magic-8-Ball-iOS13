fe8138bcfae18354e1faef2fd34a76a76be54343
Emmanuel George <Emmanuelgeorge1000@gmail.com>
2022-09-27 22:20:31 +0530

    new

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = [#imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball1")]
+    
+    @IBOutlet weak var predictionBall: UIImageView!
+    
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        predictionBall.image=ballArray[Int.random(in: 0...4)]
+    }
+    
 
 }
 
